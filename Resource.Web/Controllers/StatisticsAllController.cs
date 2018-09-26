899f1d33d6bda28f7b9935795085834518c13dbc
ballengsluo <luowenyuyu@qq.com>
2018-09-27 00:20:18 +0800

    nigth update

--- Resource.Web/Controllers/StatisticsAllController.cs
+++ Resource.Web/Controllers/StatisticsAllController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Resource.Model;
+using Resource.Web.Models;
+using Resource.Model.DB;
+using System.Data.SqlClient;
+using System.Data;
+using Newtonsoft.Json;
 namespace Resource.Web.Controllers
 {
-    public class StatisticsAllController : Controller
+    public class StatisticsAllController : BaseController
     {
         // GET: StatisticsAll
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Search(string park, string beginTime, string endTime)
+        {
+            if (string.IsNullOrEmpty(beginTime) || string.IsNullOrEmpty(endTime))
+            {
+                beginTime = DateTime.Now.ToString("yyyy-MM-dd");
+                endTime = DateTime.Now.ToString("yyyy-MM-dd");
+            }
+            List<SqlParameter> spList = new List<SqlParameter>
+            {
+                new SqlParameter("Park",park),
+                new SqlParameter("BeginTime",beginTime),
+                new SqlParameter("EndTime",endTime),
+                new SqlParameter("Account",user.Account)
+            };
+            DataSet ds = SQLFactory.Create().GetDataSet("Pro_StatisticsAll", CommandType.StoredProcedure, spList.ToArray());
+            var obj = JsonConvert.SerializeObject(ds.Tables[0]);
+            return Content(obj);
         }
     }
 }
--- Resource.Web/Plugins/app/js/app.statistics-all.js
+++ Resource.Web/Plugins/app/js/app.statistics-all.js
@@ -1,21 +1,75 @@
-$(function() {
-    if ($("#stime").length > 0) {
-        layui.use('laydate', function() {
-            var laydate = layui.laydate;
-            laydate.render({
-                elem: '#stime'
-            });
-        });
-    }
-    if ($("#etime").length > 0) {
-        layui.use('laydate', function() {
-            var laydate = layui.laydate;
-            laydate.render({
-                elem: '#etime'
-            });
-        });
-    }
-    layui.use('element', function() {
-        var element = layui.element;
-    });
+$(function () {
+  if ($('#stime').length > 0) {
+    layui.use('laydate', function () {
+      var laydate = layui.laydate
+      laydate.render({
+        elem: '#stime'
+      })
+    })
+  }
+  if ($('#etime').length > 0) {
+    layui.use('laydate', function () {
+      var laydate = layui.laydate
+      laydate.render({
+        elem: '#etime'
+      })
+    })
+  }
+  layui.use('element', function () {
+    var element = layui.element
+  })
+  search();
+  $("#search").trigger("click");
 })
+
+function search() {
+  $('#search').click(function () {
+    ajax('get', $('#search').attr('data-url'), {
+      park: $('#park').val(),
+      beginTime: $('#stime').val(),
+      endTime: $('#etime').val()
+    }, 'json', function (data) {
+      if (data.length > 0) {
+        $(".statistics-rate>span:first-of-type").html(0);
+        $(".statistics-num>span:first-of-type").html(0);
+        $.each(data, function (idx, item) {
+          if (item.Kind == 1) {
+            $("#rm-total").html(item.Total);
+            $("#rm-rent").html(item.Rent);
+            $("#rm-rentRate").html(item.RentRate);
+            $("#rm-self").html(item.Self);
+            $("#rm-selfRate").html(item.SelfRate);
+            $("#rm-free").html(item.Free);
+            $("#rm-freeRate").html(item.FreeRate);
+          } else if (item.Kind == 2) {
+            $("#wp-total").html(item.Total);
+            $("#wp-rent").html(item.Rent);
+            $("#wp-rentRate").html(item.RentRate);
+            $("#wp-self").html(item.Self);
+            $("#wp-selfRate").html(item.SelfRate);
+            $("#wp-free").html(item.Free);
+            $("#wp-freeRate").html(item.FreeRate);
+          } else if (item.Kind == 3) {
+            $("#cr-total").html(item.Total);
+            $("#cr-rent").html(item.Rent);
+            $("#cr-rentRate").html(item.RentRate);
+            $("#cr-self").html(item.Self);
+            $("#cr-selfRate").html(item.SelfRate);
+            $("#cr-free").html(item.Free);
+            $("#cr-freeRate").html(item.FreeRate);
+          } else if (item.Kind == 4) {
+            $("#ad-total").html(item.Total);
+            $("#ad-rent").html(item.Rent);
+            $("#ad-rentRate").html(item.RentRate);
+            $("#ad-self").html(item.Self);
+            $("#ad-selfRate").html(item.SelfRate);
+            $("#ad-free").html(item.Free);
+            $("#ad-freeRate").html(item.FreeRate);
+          }
+        })
+      } else {
+        layer.msg("获取数据失败！");
+      }
+    })
+  })
+}
