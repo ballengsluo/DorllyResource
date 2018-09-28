0a2c6d3feeee8dd983ad533fa57a2c7aa2a2c977
ballengsluo <luowenyuyu@qq.com>
2018-09-28 18:32:50 +0800

    0928update

--- Resource.Web/Plugins/app/js/app.statistics-all.js
+++ Resource.Web/Plugins/app/js/app.statistics-all.js
@@ -36,40 +36,39 @@ function graph() {
     // alert($("#rm").outerHeight());
     // alert($("#rm").outerWidth());
     // $("#statistics-graph").append("<div class='col-lg-6 col-md-6 col-sm-6' id='rm'></div>");
-    $("#statistics-graph").html("<div class='col-lg-6 col-md-6 col-sm-6' id='rm'></div>");
-    alert($("#statistics-graph").outerHeight());
-    alert($("#statistics-graph").outerWidth());
-    var myChart = echarts.init(document.getElementById('#rm'));
+    // $("#statistics-graph").html("<div class='col-lg-6 col-md-6 col-sm-6' id='rm'></div>");
+    // alert($("#statistics-graph").outerHeight());
+    // alert($("#statistics-graph").outerWidth());
+    // alert($("#graph1").outerHeight());
+    // alert($("#graph1").outerWidth());
+    var myChart = echarts.init(document.getElementById('graph1'));
     var option = {
         title: {
-            text: '天气情况统计',
-            subtext: '虚构数据',
-            left: 'center'
+            text: '房屋使用率',
+            left: '10%'
         },
         tooltip: {
             trigger: 'item',
             formatter: "{b} : {c} ({d}%)"
         },
+
         legend: {
-            // orient: 'vertical',
-            // top: 'middle',
-            bottom: 30,
-            left: 'center',
-            data: ['西凉', '益州', '兖州', '荆州', '幽州', 'aa']
+            orient: 'vertical',
+            top: '10%',
+            bottom: 10,
+            left: '10%',
+            data: ['客户租赁', '内部使用', '空置']
         },
         series: [{
             type: 'pie',
-            radius: '65%',
-            center: ['50%', '50%'],
+            radius: '70%',
+            center: ['60%', '50%'],
             selectedMode: 'single',
-            data: [{
-                    value: 1548,
-                    name: '幽州',
-                },
-                { value: 535, name: '荆州' },
-                { value: 510, name: '兖州' },
-                { value: 634, name: '益州' },
-                { value: 735, name: '西凉' }
+            label: { position: 'outside' },
+            data: [
+                { value: 68527.67, name: '客户租赁' },
+                { value: 330, name: '内部使用' },
+                { value: 59489.66, name: '空置' }
             ],
             itemStyle: {
                 emphasis: {
@@ -81,8 +80,8 @@ function graph() {
         }]
     };
     myChart.setOption(option);
-    $(".layui-tab-title>li:first-of-type").trigger("click");
-
+    $("#statistics-graph").append($("#graph>div"));
+    $("#graph>div").remove();
 }
 
 function search() {
@@ -92,6 +91,7 @@ function search() {
             beginTime: $('#stime').val(),
             endTime: $('#etime').val()
         }, 'json', function(data) {
+            console.log(data);
             if (data.length > 0) {
                 $(".statistics-rate>span:first-of-type").html(0);
                 $(".statistics-num>span:first-of-type").html(0);
@@ -129,11 +129,72 @@ function search() {
                         $("#ad-free").html(item.Free);
                         $("#ad-freeRate").html(item.FreeRate);
                     }
+                    var data = new Array();
+                    data.push({ name: "客户租赁", num: item.Rent, rate: item.RentRate });
+                    data.push({ name: "内部使用", num: item.Self, rate: item.SelfRate });
+                    data.push({ name: "空置", num: item.Free, rate: item.FreeRate });
+                    g2("grapy" + idx, data);
                 });
-                graph();
+
             } else {
                 layer.msg("获取数据失败！");
             }
         });
     });
+}
+
+function g2(id, data) {
+    $("#statistics-graph").append("<div class='col-lg-6 col-md-6 col-sm-6'><h4>dddd</h4><div id='" + id + "' style='width:" + $(".container").width() / 2 + "px;'></div></div>");
+    var chart = new G2.Chart({
+        container: id,
+        height: 300,
+        forceFit: true
+    });
+    chart.source(data
+        // , {
+        // percent: {
+        //     formatter: function formatter(val) {
+        //         val = val * 100 + '%';
+        //         return val;
+        //     }
+        //     }
+        // }
+    );
+    chart.coord('theta', {
+        radius: 1
+    });
+    chart.tooltip({
+        showTitle: false,
+        itemTpl: '<li>{name}: {value}</li>'
+    });
+    chart.legend("name", {
+        position: 'left-middle',
+        itemGap: 40
+            // title: {
+            //     text: 'ad',
+            //     textAlign: 'start', // 文本对齐方向，可取值为： start middle end
+            //     fill: '#404040', // 文本的颜色
+            //     fontSize: '12', // 文本大小
+            //     fontWeight: 'bold', // 文本粗细
+            //     rotate: 30, // 文本旋转角度，以角度为单位，仅当 autoRotate 为 false 时生效
+            //     textBaseline: 'top' // 文本基准线，可取 top middle bottom，默认为middle
+            // }
+    });
+    chart.intervalStack().position('rate').color('name').label('rate', {
+        offset: 5,
+        formatter: (val, item) => {
+            return item.point.name + ': ' + val + '%';
+        }
+    }).tooltip('name*rate', function(item, percent) {
+        percent = percent + '%';
+        return {
+            name: item,
+            value: percent
+        };
+    }).style({
+        lineWidth: 1,
+        stroke: '#fff'
+    });
+    chart.render();
+    $("#statistics-graph>div>div").css("width", "");
 }
